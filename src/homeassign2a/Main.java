e9dbd8dc9aafc242ab8432f46ee673d6565d7195
Huiren Woo <giantcrabby@gmail.com>
2020-02-22 05:53:46 +0800

    Change to use try

--- src/homeassign2a/Main.java
+++ src/homeassign2a/Main.java
@@ -1,61 +1,55 @@
 package homeassign2a;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
  * @author WOO HUIREN ( A0202242B )
  */
 public class Main {
-    private static String[] stringArray;
-    private static ArrayList<LinkedList<Integer>> linkedListArray;
-
     public static void main(String[] args) throws IOException {
-        final var br = new BufferedReader(new InputStreamReader(System.in));
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int stringCount = Integer.parseInt(br.readLine());
+            int stringOpCount = stringCount - 1;
+            List<LinkedList<Integer>> linkedListArray = new ArrayList<>(stringCount);
+            String[] stringArray = new String[stringCount];
 
-        final int stringCount = Integer.parseInt(br.readLine());
-        final int stringOpCount = stringCount - 1;
-        linkedListArray = new ArrayList<>(stringCount);
-        stringArray = new String[stringCount];
+            // read strings
+            // start from index 0
+            for (int i = 0; i < stringCount; i++) {
+                stringArray[i] = br.readLine();
+                linkedListArray.add(new LinkedList<>());
+            }
 
+            int finalOpId = 0;
+            // read and carry out operations
+            for (int i = 0; i < stringOpCount; i++) {
+                // 0 based index
+                StringTokenizer operationLine = new StringTokenizer(br.readLine());
+                int operationA = Integer.parseInt(operationLine.nextToken(), 10) - 1;
+                Integer operationB = Integer.parseInt(operationLine.nextToken(), 10) - 1;
 
-        // read strings
-        // start from index 0
-        for (int i = 0; i < stringCount; i++) {
-            stringArray[i] = br.readLine();
-            linkedListArray.add(new LinkedList<>());
+                linkedListArray.get(operationA).add(operationB);
+
+                finalOpId = operationA;
+            }
+
+            // Begin retrieval
+            retrieveRecursively(finalOpId, linkedListArray, stringArray);
         }
-
-        int finalOpId = 0;
-        // read and carry out operations
-        for (int i = 0; i < stringOpCount; i++) {
-            // 0 based index
-            final var operationLine = new StringTokenizer(br.readLine());
-            final var operationA = Integer.parseInt(operationLine.nextToken()) - 1;
-            final var operationB = Integer.parseInt(operationLine.nextToken()) - 1;
-
-            linkedListArray.get(operationA).add(operationB);
-
-            finalOpId = operationA;
-        }
-
-        br.close();
-
-        // Begin retrieval
-        retrieveRecursively(finalOpId);
     }
 
     // Fetch itself recursively
-    private static void retrieveRecursively(Integer id) {
-        final var currentLinkedList = linkedListArray.get(id);
+    private static void retrieveRecursively(Integer id, List<LinkedList<Integer>> linkedListArray, String[] stringArray) {
+        LinkedList<Integer> currentLinkedList = linkedListArray.get(id);
 
         System.out.print(stringArray[id]);
-        for (Integer i : currentLinkedList) {
-            retrieveRecursively(i);
-        }
+        currentLinkedList.forEach(i -> retrieveRecursively(i, linkedListArray, stringArray));
     }
 }
