faec1ba17e2e99037b208a0a674514a291b70244
Huiren Woo <giantcrabby@gmail.com>
2020-02-22 03:08:19 +0800

    Change to use linkedlist

--- src/homeassign2a/Main.java
+++ src/homeassign2a/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 /**
@@ -15,15 +16,18 @@ public class Main {
 
         final int stringCount = Integer.parseInt(br.readLine());
         final int stringOpCount = stringCount - 1;
-
-        final var builderArrayList = new ArrayList<StringBuilder>(stringCount);
+        final var linkedListArray = new ArrayList<LinkedList<Integer>>(stringCount);
+        final var builderArrayList = new String[stringCount];
 
         // read strings
         // start from index 0
         for (int i = 0; i < stringCount; i++) {
-            builderArrayList.add(new StringBuilder(br.readLine()));
+            builderArrayList[i] = br.readLine();
+            final var newLinkedList = new LinkedList<Integer>();
+            linkedListArray.add(newLinkedList);
         }
 
+        int finalOpId = 0;
         // read and carry out operations
         for (int i = 0; i < stringOpCount; i++) {
             // 0 based index
@@ -31,11 +35,24 @@ public class Main {
             final int operationA = Integer.parseInt(operationLine.nextToken()) - 1;
             final int operationB = Integer.parseInt(operationLine.nextToken()) - 1;
 
-            final StringBuilder operationBString = builderArrayList.get(operationB);
-            builderArrayList.get(operationA).append(operationBString);
-            builderArrayList.get(operationB).delete(0, operationBString.length());
+            linkedListArray.get(operationA).add(operationB);
+
+            finalOpId = operationA;
         }
 
-        builderArrayList.forEach(v -> System.out.print(v.toString()));
+        br.close();
+
+        // Begin retrieval
+        retrieveRecursively(linkedListArray, builderArrayList, finalOpId);
+    }
+
+    // Fetch itself recursively
+    private static void retrieveRecursively(ArrayList<LinkedList<Integer>> list, String[] stringList, int id) {
+        final var currentLinkedList = list.get(id);
+
+        System.out.print(stringList[id]);
+        for (Integer i : currentLinkedList) {
+            retrieveRecursively(list, stringList, i);
+        }
     }
 }
