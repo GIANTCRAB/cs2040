14c2ef4dba367bd12378424778dcf7888e2a6b75
Huiren Woo <giantcrabby@gmail.com>
2020-02-21 18:43:00 +0800

    Change to array list

--- src/homeassign2a/Main.java
+++ src/homeassign2a/Main.java
@@ -3,34 +3,38 @@ package homeassign2a;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+/**
+ * @author WOO HUIREN ( A0202242B )
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
-        final var stringMap = new HashMap<Integer, StringBuilder>();
+        final var stringMap = new ArrayList<StringBuilder>();
         final var br = new BufferedReader(new InputStreamReader(System.in));
 
         final int stringCount = Integer.parseInt(br.readLine());
         final int stringOpCount = stringCount - 1;
 
         // read strings
-        // start from index 1
-        for (int i = 1; i <= stringCount; i++) {
-            stringMap.put(i, new StringBuilder(br.readLine()));
+        // start from index 0
+        for (int i = 0; i < stringCount; i++) {
+            stringMap.add(new StringBuilder(br.readLine()));
         }
 
         // read and carry out operations
         for (int i = 0; i < stringOpCount; i++) {
+            // 0 based index
             final StringTokenizer operationLine = new StringTokenizer(br.readLine());
-            final int operationA = Integer.parseInt(operationLine.nextToken());
-            final int operationB = Integer.parseInt(operationLine.nextToken());
+            final int operationA = Integer.parseInt(operationLine.nextToken()) - 1;
+            final int operationB = Integer.parseInt(operationLine.nextToken()) - 1;
 
-            stringMap.get(operationA).append(stringMap.get(operationB));
-
-            stringMap.replace(operationB, new StringBuilder(""));
+            final StringBuilder operationBString = stringMap.get(operationB);
+            stringMap.get(operationA).append(operationBString);
+            stringMap.get(operationB).delete(0, operationBString.length());
         }
 
-        stringMap.forEach((k, v) -> System.out.print(v));
+        stringMap.forEach(v -> System.out.print(v.toString()));
     }
 }
