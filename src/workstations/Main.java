ba3850e18f3ad6517bc3e3837e52745654e85ed8
Huiren Woo <giantcrabby@gmail.com>
2020-03-13 13:08:25 +0800

    Refactor code pieces

--- src/workstations/Main.java
+++ src/workstations/Main.java
@@ -9,57 +9,68 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int unlockSavings = 0;
-            int currentlyAvail = 0;
+            final Queue<ResearcherEvent> researcherQueue = processInput(br);
 
-            final var inputInfo = new StringTokenizer(br.readLine());
-            final int numberOfResearchers = Integer.parseInt(inputInfo.nextToken());
-            final int inactivityLockMinutes = Integer.parseInt(inputInfo.nextToken());
-
-            final Queue<ResearcherEvent> researcherQueue = new PriorityQueue<>(numberOfResearchers * 3, new ResearcherEventComparator());
-
-            for (int i = 0; i < numberOfResearchers; i++) {
-                var researcherInfo = new StringTokenizer(br.readLine());
-                var arrivalTime = Integer.parseInt(researcherInfo.nextToken());
-                var researcherArrivalEvent = new ResearcherEvent(arrivalTime, EventTypes.ARRIVAL);
-                var departureTime = Integer.parseInt(researcherInfo.nextToken()) + arrivalTime;
-                var researcherDepartureEvent = new ResearcherEvent(departureTime, EventTypes.DEPARTURE);
-                var researcherLockEvent = new ResearcherEvent(departureTime + inactivityLockMinutes, EventTypes.LOCK);
-
-                researcherQueue.add(researcherArrivalEvent);
-                researcherQueue.add(researcherDepartureEvent);
-                researcherQueue.add(researcherLockEvent);
-            }
-
-            while (researcherQueue.size() > 0) {
-                var researcherEvent = researcherQueue.poll();
-
-                if (researcherEvent.eventType == EventTypes.ARRIVAL) {
-                    if (currentlyAvail > 0) {
-                        // use unlocked
-                        unlockSavings++;
-                        currentlyAvail--;
-                    } else {
-                        // unlock one computer
-                        currentlyAvail++;
-                    }
-                }
-
-                if (researcherEvent.eventType == EventTypes.DEPARTURE) {
-                    currentlyAvail++;
-                }
-
-                if (researcherEvent.eventType == EventTypes.LOCK) {
-                    currentlyAvail--;
-                }
-            }
+            final int totalUnlockSavings = processQueue(researcherQueue);
 
             try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FileDescriptor.out), StandardCharsets.US_ASCII), 512)) {
-                bw.write(String.valueOf(unlockSavings));
+                bw.write(String.valueOf(totalUnlockSavings));
                 bw.flush();
             }
         }
+    }
+
+    public static Queue<ResearcherEvent> processInput(BufferedReader br) throws IOException {
+        var inputInfo = new StringTokenizer(br.readLine());
+        int numberOfResearchers = Integer.parseInt(inputInfo.nextToken());
+        int inactivityLockMinutes = Integer.parseInt(inputInfo.nextToken());
+
+        Queue<ResearcherEvent> researcherEventQueue = new PriorityQueue<>(numberOfResearchers * 3, new ResearcherEventComparator());
+
+        for (int i = 0; i < numberOfResearchers; i++) {
+            var researcherInfo = new StringTokenizer(br.readLine());
+            var arrivalTime = Integer.parseInt(researcherInfo.nextToken());
+            var researcherArrivalEvent = new ResearcherEvent(arrivalTime, EventTypes.ARRIVAL);
+            var departureTime = Integer.parseInt(researcherInfo.nextToken()) + arrivalTime;
+            var researcherDepartureEvent = new ResearcherEvent(departureTime, EventTypes.DEPARTURE);
+            var researcherLockEvent = new ResearcherEvent(departureTime + inactivityLockMinutes, EventTypes.LOCK);
+
+            researcherEventQueue.add(researcherArrivalEvent);
+            researcherEventQueue.add(researcherDepartureEvent);
+            researcherEventQueue.add(researcherLockEvent);
+        }
+
+        return researcherEventQueue;
+    }
+
+    public static int processQueue(Queue<ResearcherEvent> researcherQueue) {
+        int unlockSavings = 0;
+        int currentlyAvail = 0;
+
+        while (researcherQueue.size() > 0) {
+            var researcherEvent = researcherQueue.poll();
+
+            if (researcherEvent.eventType == EventTypes.ARRIVAL) {
+                if (currentlyAvail > 0) {
+                    // use unlocked, increment savings
+                    unlockSavings++;
+                    currentlyAvail--;
+                } else {
+                    // unlock one computer
+                    currentlyAvail++;
+                }
+            }
+
+            if (researcherEvent.eventType == EventTypes.DEPARTURE) {
+                currentlyAvail++;
+            }
+
+            if (researcherEvent.eventType == EventTypes.LOCK) {
+                currentlyAvail--;
+            }
+        }
+
+        return unlockSavings;
     }
 }
