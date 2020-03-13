522ed94e995519a83f8ecebd92dffb5b5176648f
Huiren Woo <giantcrabby@gmail.com>
2020-03-13 21:40:39 +0800

    Change priority for comparison

--- src/workstations/EventTimeComparator.java
+++ src/workstations/EventTimeComparator.java
@@ -7,20 +7,17 @@ public class EventTimeComparator implements Comparator<EventTime> {
     public int compare(EventTime o1, EventTime o2) {
         final int firstComparison = o1.getEventStart().compareTo(o2.getEventStart());
         if (firstComparison == 0) {
-            final int secondComparison = o1.getEventEnd().compareTo(o2.getEventEnd());
-            if (secondComparison == 0) {
-                // Free'ing should take most priority
-                if (o1.getEventType() == EventTypes.FREE) {
-                    // Don't shift since both are priority
-                    if (o2.getEventType() == EventTypes.FREE) {
-                        return 0;
-                    }
-
-                    return -1;
+            // Free'ing should take most priority
+            if (o1.getEventType() == EventTypes.FREE) {
+                // Don't shift since both are priority
+                if (o2.getEventType() == EventTypes.FREE) {
+                    return 0;
                 }
+
+                return -1;
             }
 
-            return secondComparison;
+            return o1.getEventEnd().compareTo(o2.getEventEnd());
         }
         return firstComparison;
     }
