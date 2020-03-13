fd5b3f4dc22d2fa2878760a15b910993d455cb3a
Huiren Woo <giantcrabby@gmail.com>
2020-03-13 13:08:40 +0800

    Add priority based on event

--- src/workstations/ResearcherEventComparator.java
+++ src/workstations/ResearcherEventComparator.java
@@ -5,6 +5,17 @@ import java.util.Comparator;
 public class ResearcherEventComparator implements Comparator<ResearcherEvent> {
     @Override
     public int compare(ResearcherEvent o1, ResearcherEvent o2) {
-        return o1.eventTime.compareTo(o2.eventTime);
+        var firstComparison = o1.eventTime.compareTo(o2.eventTime);
+        if (firstComparison == 0) {
+            // Departure should take most priority
+            if (o1.eventType == EventTypes.DEPARTURE && o2.eventType != EventTypes.DEPARTURE) {
+                return -1;
+            }
+            // Arrival should take priority over LOCK
+            if (o1.eventType == EventTypes.ARRIVAL && o2.eventType == EventTypes.LOCK) {
+                return -1;
+            }
+        }
+        return firstComparison;
     }
 }
