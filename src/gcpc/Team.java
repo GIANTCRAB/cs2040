96e8319e0eb6643302445fda30ae72025e05b719
Huiren Woo <giantcrabby@gmail.com>
2020-04-03 03:50:46 +0800

    Add team

---
+++ src/gcpc/Team.java
@@ -0,0 +1,27 @@
+package gcpc;
+
+public class Team {
+    private final Integer id;
+    private Integer score;
+    private Integer penalty;
+
+    Team(Integer id) {
+        this.id = id;
+        this.score = 0;
+        this.penalty = 0;
+    }
+
+    public Team incrementScore() {
+        this.score++;
+        return this;
+    }
+
+    public Integer getScore() {
+        return this.score;
+    }
+
+    public Team addPenalty(Integer penalty) {
+        this.penalty += penalty;
+        return this;
+    }
+}
