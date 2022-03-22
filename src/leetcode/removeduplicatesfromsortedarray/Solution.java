5cde12aa10068a21dd97e564955e1f715f1094d8
Huiren Woo <giantcrabby@gmail.com>
2022-03-22 23:08:37 +0800

    Optimize memory usage

--- src/leetcode/removeduplicatesfromsortedarray/Solution.java
+++ src/leetcode/removeduplicatesfromsortedarray/Solution.java
@@ -1,24 +1,21 @@
 package leetcode.removeduplicatesfromsortedarray;
 
 // This uses in place methods such as a modified insertion sort
-// TODO: Make it faster
+// It uses less memory but it is quite slow.
+// TODO: Make it faster while maintaining low memory footprint
 class Solution {
+    final int BLANK_VALUE = 1000;
+
     public int removeDuplicates(int[] nums) {
-        final int BLANK_VALUE = 1000;
-        boolean doLoop = true;
-        int currentIndex = 0;
-        int removals = 0;
-        Integer previousValue = null;
-        Integer currentValue;
-        while (doLoop) {
-            currentValue = nums[currentIndex];
-            // skip if on blank
-            if (currentValue.equals(BLANK_VALUE)) {
-                currentIndex++;
-                continue;
-            }
-            if (previousValue != null) {
-                if (previousValue.equals(currentValue)) {
+        if (nums.length > 1) {
+            boolean doLoop = true;
+            int currentIndex = 1;
+            int removals = 0;
+            int previousValue = nums[currentIndex - 1];
+            int currentValue;
+            while (doLoop) {
+                currentValue = nums[currentIndex];
+                if (previousValue == currentValue) {
                     // Remove current value by setting it to BLANK_VALUE
                     nums[currentIndex] = BLANK_VALUE;
                     currentIndex++;
@@ -27,29 +24,28 @@ class Solution {
                     previousValue = nums[currentIndex];
                     currentIndex++;
                 }
-            } else {
-                previousValue = nums[currentIndex];
-                currentIndex++;
+
+                if (currentIndex == nums.length) {
+                    doLoop = false;
+                }
             }
 
-            if (currentIndex == nums.length) {
-                doLoop = false;
-            }
+            modifiedInsertionSort(nums);
+
+            return nums.length - removals;
         }
 
-        modifiedInsertionSort(nums);
-
-        return nums.length - removals;
+        return nums.length;
     }
 
     public void modifiedInsertionSort(int[] nums) {
-        if (nums.length > 1) {
-            boolean doSwap = true;
-            int currentIndex = 0;
-            int forwardIndex = 1;
-            while (doSwap) {
-                final int currentValue = nums[currentIndex];
-                final int forwardValue = nums[forwardIndex];
+        boolean doSwap = true;
+        int currentIndex = 0;
+        int forwardIndex = 1;
+        while (doSwap) {
+            final int currentValue = nums[currentIndex];
+            final int forwardValue = nums[forwardIndex];
+            if (currentValue == BLANK_VALUE) {
                 // swap when larger
                 if (currentValue > forwardValue) {
                     nums[forwardIndex] = currentValue;
@@ -65,10 +61,13 @@ class Solution {
                         forwardIndex = currentIndex + 1;
                     }
                 }
+            } else {
+                currentIndex++;
+                forwardIndex = currentIndex + 1;
+            }
 
-                if (currentIndex + 1 == nums.length) {
-                    doSwap = false;
-                }
+            if (currentIndex + 1 == nums.length) {
+                doSwap = false;
             }
         }
     }
