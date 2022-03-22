254b029acd412e55210048d84ad4073adc5ccc17
Huiren Woo <giantcrabby@gmail.com>
2022-03-23 00:59:58 +0800

    Fix conditionals in find-first-and-last-position-of-element-in-sorted-array

--- src/leetcode/findfirstandlastpositionofelementinsortedarray/Solution.java
+++ src/leetcode/findfirstandlastpositionofelementinsortedarray/Solution.java
@@ -3,51 +3,55 @@ package leetcode.findfirstandlastpositionofelementinsortedarray;
 // Very similar to searchinrotatedsortedarray
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length > 1) {
+        if (nums.length > 1) {
             boolean leftHalt = false;
             boolean rightHalt = false;
             int mostLeftIndex = 0;
             int mostRightIndex = nums.length - 1;
             while (true) {
-                if(!leftHalt) {
+                if (!leftHalt) {
                     if (nums[mostLeftIndex] < target) {
                         // move leftIndex forward
                         mostLeftIndex++;
-                    }
-                    if (nums[mostLeftIndex] == target) {
-                        // halt the left index
-                        leftHalt = true;
-                    }
-                    if (nums[mostLeftIndex] > target) {
-                        // not found
-                        break;
+                    } else {
+                        if (nums[mostLeftIndex] == target) {
+                            // halt the left index
+                            leftHalt = true;
+                        } else {
+                            if (nums[mostLeftIndex] > target) {
+                                // not found
+                                break;
+                            }
+                        }
                     }
                 }
 
-                if(!rightHalt) {
+                if (!rightHalt) {
                     if (nums[mostRightIndex] > target) {
                         // move rightIndex backward
                         mostRightIndex--;
-                    }
-                    if (nums[mostRightIndex] == target) {
-                        // halt the right index
-                        rightHalt = true;
-                    }
-                    if (nums[mostRightIndex] < target) {
-                        // not found
-                        break;
+                    } else {
+                        if (nums[mostRightIndex] == target) {
+                            // halt the right index
+                            rightHalt = true;
+                        } else {
+                            if (nums[mostRightIndex] < target) {
+                                // not found
+                                break;
+                            }
+                        }
                     }
                 }
 
-                if(leftHalt && rightHalt) {
-                    return new int[] {mostLeftIndex, mostRightIndex};
+                if (leftHalt && rightHalt) {
+                    return new int[]{mostLeftIndex, mostRightIndex};
                 }
             }
         } else {
-            if(nums.length > 0 && nums[0] == target) {
-                return new int[] {0, 0};
+            if (nums.length > 0 && nums[0] == target) {
+                return new int[]{0, 0};
             }
         }
-        return new int[] {-1, -1};
+        return new int[]{-1, -1};
     }
 }
