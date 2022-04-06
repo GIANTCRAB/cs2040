6d94ff9dea6e3d6f3fba05c7eac1b573cb6eda1b
Huiren Woo <giantcrabby@gmail.com>
2022-04-06 10:09:39 +0800

    Update comments for combinations

--- src/leetcode/combinations/Solution.java
+++ src/leetcode/combinations/Solution.java
@@ -7,29 +7,31 @@ import java.util.stream.IntStream;
 
 class Solution {
     public List<List<Integer>> combine(int n, int maxSizeLeft) {
-        // form the smallest possible number with combination first
         final List<List<Integer>> fullList = new ArrayList<>();
+        // Form the smallest possible number with combination first
         final List<Integer> leftArray = IntStream.rangeClosed(1, maxSizeLeft).collect(ArrayList::new, List::add, List::addAll);
+        int currentIndexLeft = maxSizeLeft - 1;
         final List<Integer> rightArray = IntStream.rangeClosed(maxSizeLeft, n).collect(ArrayList::new, List::add, List::addAll);
         fullList.add(new ArrayList<>(leftArray));
-        int currentIndexLeft = maxSizeLeft - 1;
 
         while (currentIndexLeft != -1) {
             final boolean hasMutated = this.compareAndReplace(currentIndexLeft, currentIndexLeft, leftArray, rightArray);
 
             if (!hasMutated) {
                 while (true) {
-                    // shift left index
                     currentIndexLeft--;
+                    // Is it still possible to mutate? Or are we out of options?
                     if (currentIndexLeft != -1) {
-                        // Remove everything behind the currentIndexLeft
                         this.removeBack(currentIndexLeft, leftArray, rightArray);
                         final boolean toReset = this.compareAndReplace(currentIndexLeft, currentIndexLeft, leftArray, rightArray);
-                        // reset all left values after index in leftArray
-                        // move from left to right manner
+                        // Reset all left values after index in leftArray
+                        // Move from left to right manner
                         for (int resetIndexLeft = currentIndexLeft; resetIndexLeft < maxSizeLeft - 1; resetIndexLeft++) {
                             this.compareAndAdd(resetIndexLeft, leftArray, rightArray);
                         }
+                        // Check if everything has been added back
+                        // If not everything has been added back, this means we need to increase the depth of mutations to make
+                        // Depth of mutation is controlled by currentIndexLeft
                         if (leftArray.size() == maxSizeLeft) {
                             // everything added back just fine
                             if (toReset) {
@@ -66,7 +68,6 @@ class Solution {
                 leftArray.set(leftIndexToReplace, rightValue);
                 rightArray.remove(i);
                 rightArray.add(tempValue);
-                // sort right array
                 rightArray.sort(Comparator.naturalOrder());
                 hasMutated = true;
                 break;
@@ -76,6 +77,7 @@ class Solution {
         return hasMutated;
     }
 
+    // Everything after the leftIndexSeparator is removed
     private void removeBack(int leftIndexSeparator, List<Integer> leftArray, List<Integer> rightArray) {
         while (leftIndexSeparator + 1 < leftArray.size()) {
             final int tempValue = leftArray.get(leftIndexSeparator + 1);
@@ -83,10 +85,10 @@ class Solution {
             rightArray.add(tempValue);
         }
 
-        // sort right array
         rightArray.sort(Comparator.naturalOrder());
     }
 
+    // Keep adding to the leftArray until it is full or until we are out of options
     private void compareAndAdd(int leftIndexToCompare, List<Integer> leftArray, List<Integer> rightArray) {
         for (int i = 0; i < rightArray.size(); i++) {
             final int rightValue = rightArray.get(i);
@@ -94,7 +96,6 @@ class Solution {
             if (leftValue < rightValue) {
                 leftArray.add(rightValue);
                 rightArray.remove(i);
-                // sort right array
                 rightArray.sort(Comparator.naturalOrder());
                 break;
             }
