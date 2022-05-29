855369fded4d3d1a9df87e210a598519f284cf6e
Huiren Woo <giantcrabby@gmail.com>
2022-05-29 13:17:52 +0800

    Resolve container-with-most-water TLE

--- src/leetcode/containerwithmostwater/Solution.java
+++ src/leetcode/containerwithmostwater/Solution.java
@@ -1,18 +1,56 @@
 package leetcode.containerwithmostwater;
 
-// TODO: Fix Time Limit Exceeded
+// TODO: Make it faster
 public class Solution {
     public int maxArea(int[] height) {
+        final int heightLength = height.length;
+        // min value, distance
         int largestArea = 0;
-        for (int i = 0; i < height.length; i++) {
-            final int startPointHeight = height[i];
-            for (int j = (i + 1); j < height.length; j++) {
-                final int endPointHeight = height[j];
-                final int distanceBetweenPoints = j - i;
-                final int calculatedArea = Math.min(startPointHeight, endPointHeight) * distanceBetweenPoints;
 
-                if (calculatedArea > largestArea) {
-                    largestArea = calculatedArea;
+        // scan from left to right
+        for (int leftIndex = 0; leftIndex < heightLength; leftIndex++) {
+            final int startPointHeight = height[leftIndex];
+
+            int furthestIndex = heightLength - 1;
+            while (true) {
+                int furthestValue = height[furthestIndex];
+                if (furthestValue >= startPointHeight) {
+                    final int distanceBetweenIndex = furthestIndex - leftIndex;
+                    final int area = distanceBetweenIndex * startPointHeight;
+                    if (area > largestArea) {
+                        largestArea = area;
+                    }
+                    break;
+                } else {
+                    if (furthestIndex > leftIndex) {
+                        furthestIndex--;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        // scan right to left
+        for (int rightIndex = heightLength - 1; rightIndex > -1; rightIndex--) {
+            final int startPointHeight = height[rightIndex];
+
+            int furthestIndex = 0;
+            while (true) {
+                int furthestValue = height[furthestIndex];
+                if (furthestValue >= startPointHeight) {
+                    final int distanceBetweenIndex = rightIndex - furthestIndex;
+                    final int area = distanceBetweenIndex * startPointHeight;
+                    if (area > largestArea) {
+                        largestArea = area;
+                    }
+                    break;
+                } else {
+                    if (furthestIndex < rightIndex) {
+                        furthestIndex++;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
