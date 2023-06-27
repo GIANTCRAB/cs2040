cac532eacf1a5e7815138108a8076e912def329e
Huiren Woo <giantcrabby@gmail.com>
2023-06-28 03:01:55 +0800

    Improve threesum solution by removing hash set usage

--- src/leetcode/threesum/Solution.java
+++ src/leetcode/threesum/Solution.java
@@ -2,16 +2,27 @@ package leetcode.threesum;
 
 import java.util.*;
 
-// TODO: Improve run time performance
+// TODO: Improve memory/space usage?
 class Solution {
     public List<List<Integer>> threeSum(int[] sortedArray) {
-        final Set<List<Integer>> fullSet = new HashSet<>();
+        final List<List<Integer>> fullSet = new ArrayList<>();
         // Sort the given array first
         Arrays.sort(sortedArray);
-        int firstFixedIndex = 0;
-        int firstMovingIndex = 1;
-        int secondMovingIndex = sortedArray.length - 1;
-        while(firstFixedIndex + 2 < sortedArray.length) {
+        int firstMovingIndex;
+        int secondMovingIndex;
+        for(int firstFixedIndex = 0; firstFixedIndex + 2 < sortedArray.length; firstFixedIndex++) {
+            firstMovingIndex = firstFixedIndex + 1;
+            secondMovingIndex = sortedArray.length - 1;
+
+            // If the number is the same as the one behind, we can skip to the next loop
+            if(firstFixedIndex > 0 && (sortedArray[firstFixedIndex] == sortedArray[firstFixedIndex-1])) {
+                continue;
+            }
+            // We can stop the loop if the left side is no longer negative
+            if (sortedArray[firstFixedIndex] + sortedArray[firstMovingIndex] > 0) {
+                break;
+            }
+
             while(firstMovingIndex < secondMovingIndex) {
                 final int sumResult = sortedArray[firstFixedIndex] + sortedArray[firstMovingIndex] + sortedArray[secondMovingIndex];
                 if(sumResult == 0) {
@@ -20,7 +31,12 @@ class Solution {
                     newList.add(sortedArray[firstMovingIndex]);
                     newList.add(sortedArray[secondMovingIndex]);
                     fullSet.add(newList);
-                    firstMovingIndex++;
+                    do {
+                        firstMovingIndex++;
+                    } while (firstMovingIndex < secondMovingIndex && sortedArray[firstMovingIndex - 1] == sortedArray[firstMovingIndex]);
+                    do {
+                        secondMovingIndex--;
+                    } while (secondMovingIndex > firstMovingIndex && sortedArray[secondMovingIndex + 1] == sortedArray[secondMovingIndex]);
                 } else {
                     if (sumResult < 0) {
                         // negative right now, need to move firstMovingIndex forward towards a positive number
@@ -31,11 +47,8 @@ class Solution {
                     }
                 }
             }
-            firstFixedIndex++;
-            firstMovingIndex = firstFixedIndex + 1;
-            secondMovingIndex = sortedArray.length - 1;
         }
 
-        return new ArrayList<>(fullSet);
+        return fullSet;
     }
 }
