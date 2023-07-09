f847ab703f4caaccb592968a50a8b1c8840aa3bc
Huiren Woo <giantcrabby@gmail.com>
2023-07-10 02:39:37 +0800

    Improve precision of approximation in algorithmstuff

--- src/algorithmstuff/sqrtapproximation/Main.java
+++ src/algorithmstuff/sqrtapproximation/Main.java
@@ -1,19 +1,23 @@
 package algorithmstuff.sqrtapproximation;
 
+import java.text.DecimalFormat;
+
 public class Main {
 
     public static void main(String[] args) {
-        newtonMethod(9);
+        newtonMethod(2);
     }
 
     public static void newtonMethod(int target) {
         double currentGuess = 1;
         double currentResult;
         boolean closeEnough = false;
+        DecimalFormat df = new DecimalFormat("#.########");
+        DecimalFormat resultDf = new DecimalFormat("#.#####");
         while (!closeEnough) {
-            currentGuess = ((target / currentGuess) + currentGuess) / 2;
-            currentResult = currentGuess * currentGuess;
-            closeEnough = Math.floor(currentResult) == target || Math.ceil(currentResult) == target;
+            currentGuess = Double.parseDouble(df.format(((target / currentGuess) + currentGuess) / 2));
+            currentResult = Double.parseDouble(resultDf.format(currentGuess * currentGuess));
+            closeEnough = currentResult == target;
         }
         System.out.println(currentGuess);
     }
